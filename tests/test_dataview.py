232e37e662edac0be6d8e3e53d2ad5b61f9c08bf
AlexV <asmodehn@gmail.com>
2020-05-21 16:30:00 +0200

    adding simple test for dataview

---
+++ tests/test_dataview.py
@@ -0,0 +1,35 @@
+import pytest
+import datetime
+import pandas
+import random
+from datetime import datetime, timedelta
+
+from bokeh.models import Plot
+
+from livebokeh.datamodel import DataModel
+from livebokeh.dataview import DataView
+
+
+def test_render():
+    now = datetime.now()
+
+    # TODO : generate sample data with hypothesis
+    df = pandas.DataFrame(
+        data=[[random.randint(-10, 10), random.randint(-10, 10)], [random.randint(-10, 10), random.randint(-10, 10)]]
+        , columns=["random1", "random2"],
+        index=[now, now + timedelta(milliseconds=1)])
+
+    dm = DataModel(name="TestDataModel", data=df)
+
+    dv = DataView(model=dm)
+
+    assert dv.model == dm
+    rendered = dv()
+
+    assert isinstance(rendered, Plot)
+
+    assert dv.model == dm
+    rendered2 = dv()
+
+    assert isinstance(rendered, Plot)
+    assert rendered2 != rendered
