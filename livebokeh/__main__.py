35256800711493d57e09da2f0ac582a9666a57ed
AlexV <asmodehn@gmail.com>
2020-05-28 11:51:35 +0200

    improved running self examples properly and without bokeh warnings

--- livebokeh/__main__.py
+++ livebokeh/__main__.py
@@ -5,29 +5,39 @@ import functools
 
 from livebokeh import monosrv, datamodel, clockdata, dataview
 
-# retrieving all modules
-modls = [monosrv, datamodel, dataview, clockdata]
 
-# launching all examples, BEFORE web request
-# to make sure livebokeh code does NOT depend on doc being there
+async def main():
+    # Note we need an async main here to ensure a loop is currently running.
 
-examples = {
-    # We need to start all examples in background => async (this will endup being the server loop, but server isnt created just yet...
-    m.__name__: asyncio.run(m._internal_example())
-    for m in modls
-    if hasattr(m, "_internal_example")
-}
+    # retrieving all modules
+    modls = [monosrv, datamodel, dataview, clockdata]
 
-livebokeh_mains = {
-    m.__name__: functools.partial(m._internal_bokeh, example=examples.get(m.__name__))
-    for m in modls
-    if hasattr(m, "_internal_bokeh")
-}
+    # launching all examples, BEFORE web request
+    # to make sure livebokeh code does NOT depend on doc being there
 
-# retrieving livebokeh entry points for each module, with optional example
-apps = {"/" + m.__name__: livebokeh_mains.get(m.__name__) for m in modls}
+    examples = {
+        # We need to start all examples in background
+        # => async (this is scheduled in the current server loop)
+        m.__name__: await m._internal_example()  # we do need to get a first result from the exemple to certify startup.
+        for m in modls
+        if hasattr(m, "_internal_example")
+    }
+
+    livebokeh_mains = {
+        m.__name__: functools.partial(
+            m._internal_bokeh, example=examples.get(m.__name__)
+        )
+        for m in modls
+        if hasattr(m, "_internal_bokeh")
+    }
+
+    # retrieving livebokeh entry points for each module, with optional example
+    apps = {"/" + m.__name__: livebokeh_mains.get(m.__name__) for m in modls}
+
+    await monosrv.monosrv(apps)
+
 
 try:
-    asyncio.run(monosrv.monosrv(apps))
+    asyncio.run(main())
 except KeyboardInterrupt:
     print("Exiting...")
